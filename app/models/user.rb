a92ae62b57fa03f7f80749d378aa3e8e618ed70d
tttooommmooo <tabinn.0803.koyumayu@gmail.com>
2021-02-13 12:07:52 +0900

    ユーザ登録のバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, :last_name, :first_name,:last_name_kana, :first_name_kana, :birth_date, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: "Include both letters and numbers"
+
+  with_options  format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "Full-width characters" } do
+    validates :first_name
+    validates :last_name
+  end
+  
+  with_options  format: { with: /\A[ァ-ヶ]+\z/, message: "Full-width katakana characters" } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
 end
