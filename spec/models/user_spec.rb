6938584361b00488728045b1247ef8798f33f31f
tttooommmooo <tabinn.0803.koyumayu@gmail.com>
2021-02-13 12:49:19 +0900

    テストコード作成（example1~3）

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    it 'nicknameが空では登録できない' do
+      user = User.new(nickname: '')
+      user.valid?
+      expect(user.errors.full_messages).to include("Nickname can't be blank")
+    end
+    it 'emailが空では登録できない' do
+      user = User.new(email: '')
+      user.valid?
+      expect(user.errors.full_messages).to include("Email can't be blank")
+    end
+    it '他のユーザーとemailが重複すると登録できない' do
+      user = User.new(email: '')
+      user.valid?
+      expect(user.errors.full_messages).to include("Email has already been taken")
+    end
+  end
 end
