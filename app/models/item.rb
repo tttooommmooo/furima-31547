f41036e5a09294c989d3c3ff7701218d37fd2291
tttooommmooo <tabinn.0803.koyumayu@gmail.com>
2021-02-20 16:42:49 +0900

    出品金額のバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,4 +11,7 @@ class Item < ApplicationRecord
   validates :image, :name, :info, :price, :user_id, presence: true
 
   validates :category_id, :status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, numericality: { other_than: 1 }
+
+  validates :price, :numericality => { :greater_than_or_equal_to => 300 }
+  validates :price, :numericality => { :less_than_or_equal_to => 9999999 }
 end
