27efe5a5ea69063568d05b9ced197b26b445144d
tttooommmooo <tabinn.0803.koyumayu@gmail.com>
2021-02-20 12:12:20 +0900

    項目選択、コントローラー編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,24 @@
 class ItemsController < ApplicationController
   def index
-    
+    @items = Item.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:info,:category_id,:status_id,:shipping_fee_status_id,:prefecture_id,:scheduled_delivery_id,:price,).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
+
+  validates :name, :info, :price, :user, presence: true
+
+  validates :category_id, :status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, numericality: { other_than: 1 }
 end
