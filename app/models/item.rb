645c85015fd536e7c2307475fcf8a64f3c4375c8
tttooommmooo <tabinn.0803.koyumayu@gmail.com>
2021-02-21 09:30:47 +0900

    エラーメッセージの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,12 +10,11 @@ class Item < ApplicationRecord
 
   validates :image, :name, :info, :price, :user_id, presence: true
 
-  validates :category_id, :status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, numericality: { other_than: 1 }
+  validates :category_id, :status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, numericality: { other_than: 1, message:"Select"}
 
-  validates :price, :numericality => { :greater_than_or_equal_to => 300 }
-  validates :price, :numericality => { :less_than_or_equal_to => 9999999 }
+  validates :price, :numericality => { :greater_than_or_equal_to => 300, :less_than_or_equal_to => 9999999, message:"Out of setting range"}
 
-  with_options  format: { with: /^[0-9]+$/ } do
+  with_options  format: { with: /\A[0-9]+\z/, message:"Half-width number" } do
     validates :price
   end
   
