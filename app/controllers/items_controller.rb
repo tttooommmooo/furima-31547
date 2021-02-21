47ed2eb270fc3b95ca649d400d9ff95006214120
tttooommmooo <tabinn.0803.koyumayu@gmail.com>
2021-02-21 20:17:49 +0900

    レビューに対するparams関連の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.permit(:image,:name,:info,:category_id,:status_id,:shipping_fee_status_id,:prefecture_id,:scheduled_delivery_id,:price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image,:name,:info,:category_id,:status_id,:shipping_fee_status_id,:prefecture_id,:scheduled_delivery_id,:price).merge(user_id: current_user.id)
   end
 end
