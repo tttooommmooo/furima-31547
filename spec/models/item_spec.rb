c5d1874d440f3bbf4dbaaf5ba074a2e8bd570ce6
tttooommmooo <tabinn.0803.koyumayu@gmail.com>
2021-02-21 09:42:49 +0900

    itemモデルのテストファイル生成
    ターミナル操作でテストファイル作成
    rails g rspec:model item

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+  end
+
+  describe '商品の出品' do
+    context '出品ができるとき' do
+      it "nickname、email、password、password_confirmation、last_name、first_name、last_name_kana、first_name_kana、birth_dateが存在すれば登録できる" do
+        expect(@user).to be_valid
+      end
+      it "passwordが6文字以上であれば登録できる" do
+        @user.password = "000aaa"
+        @user.password_confirmation = "000aaa"
+        expect(@user).to be_valid
+      end
+    end
+    context '出品ができないとき' do
+      it 'nicknameが空では登録できない' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+    end
+  end
 end
