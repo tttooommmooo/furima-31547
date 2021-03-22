9e7ab9bd891be53dc307f660c9af25cf9330bb5d
tttooommmooo <tabinn.0803.koyumayu@gmail.com>
2021-03-22 20:41:20 +0900

    テストコードの修正

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -9,77 +9,82 @@ RSpec.describe OrderAddress, type: :model do
       sleep 0.1
     end
 
-    it 'すべての値が正しく入力されていれば保存できること' do
-      expect(@order_address).to be_valid
+    context '保存できる時' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+        expect(@order_address).to be_valid
+      end
+      it 'ビル名が空でも保存ができること' do
+        @order_address.building = nil
+        expect(@order_address).to be_valid
+      end 
     end
-    it 'ビル名が空でも購入ができること' do
-      @order_address.building = nil
-      expect(@order_address).to be_valid
-    end
-    it '郵便番号が空だと保存できないこと' do
-      @order_address.postal_code = ''
-      @order_address.valid?
-      expect(@order_address.errors.full_messages).to include("Postal code can't be blank")
-    end
-    it '都道府県が空だと保存できないこと' do
-      @order_address.prefecture_id = nil
-      @order_address.valid?
-      expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
-    end
-    it '都道府県が"---"だと保存できないこと' do
-      @order_address.prefecture_id = 1
-      @order_address.valid?
-      expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
-    end
-    it '市区町村が空だと保存できないこと' do
-      @order_address.city = ''
-      @order_address.valid?
-      expect(@order_address.errors.full_messages).to include("City can't be blank")
-    end
-    it '番地が空だと保存できないこと' do
-      @order_address.addresses = ''
-      @order_address.valid?
-      expect(@order_address.errors.full_messages).to include("Addresses can't be blank")
-    end
-    it '電話番号が空だと保存できないこと' do
-      @order_address.phone_number = ''
-      @order_address.valid?
-      expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
-    end
-    it '郵便番号が半角のハイフンを含んだ正しい形式でないと保存できないこと' do
-      @order_address.postal_code = '1234567'
-      @order_address.valid?
-      expect(@order_address.errors.full_messages).to include('Postal code is invalid. Include hyphen(-)')
-    end
-    it '電話番号にハイフンを含むと保存できないこと' do
-      @order_address.phone_number = '090-1234-5678'
-      @order_address.valid?
-      expect(@order_address.errors.full_messages).to include('Phone number Input only number')
-    end
-    it '電話番号は11桁以内でないと保存できないこと' do
-      @order_address.phone_number = '000000000000'
-      @order_address.valid?
-      expect(@order_address.errors.full_messages).to include('Phone number Input only number')
-    end
-    it 'phone_numberが英数混合では登録できないこと' do
-      @order_address.phone_number = '000000ooooo'
-      @order_address.valid?
-      expect(@order_address.errors.full_messages).to include('Phone number Input only number')
-    end
-    it "tokenが空では登録できないこと" do
-      @order_address.token = nil
-      @order_address.valid?
-      expect(@order_address.errors.full_messages).to include("Token can't be blank")
-    end
-    it "user_idが空だと購入できない" do
-      @order_address.user_id = nil
-      @order_address.valid?
-      expect(@order_address.errors.full_messages).to include("User can't be blank")
-    end
-    it "item_idが空だと購入できない" do
-      @order_address.item_id = nil
-      @order_address.valid?
-      expect(@order_address.errors.full_messages).to include("Item can't be blank")
+
+    context '保存できない時' do
+      it '郵便番号が空だと保存できないこと' do
+        @order_address.postal_code = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it '都道府県が空だと保存できないこと' do
+        @order_address.prefecture_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it '都道府県が"---"だと保存できないこと' do
+        @order_address.prefecture_id = 1
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it '市区町村が空だと保存できないこと' do
+        @order_address.city = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("City can't be blank")
+      end
+      it '番地が空だと保存できないこと' do
+        @order_address.addresses = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Addresses can't be blank")
+      end
+      it '電話番号が空だと保存できないこと' do
+        @order_address.phone_number = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it '郵便番号が半角のハイフンを含んだ正しい形式でないと保存できないこと' do
+        @order_address.postal_code = '1234567'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include('Postal code is invalid. Include hyphen(-)')
+      end
+      it '電話番号にハイフンを含むと保存できないこと' do
+        @order_address.phone_number = '090-1234-5678'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include('Phone number Input only number')
+      end
+      it '電話番号は11桁以内でないと保存できないこと' do
+        @order_address.phone_number = '000000000000'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include('Phone number Input only number')
+      end
+      it 'phone_numberが英数混合では保存できないこと' do
+        @order_address.phone_number = '000000ooooo'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include('Phone number Input only number')
+      end
+      it "tokenが空では保存できないこと" do
+        @order_address.token = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Token can't be blank")
+      end
+      it "user_idが空だと保存できない" do
+        @order_address.user_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("User can't be blank")
+      end
+      it "item_idが空だと保存できない" do
+        @order_address.item_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Item can't be blank")
+      end 
     end
   end
 end
